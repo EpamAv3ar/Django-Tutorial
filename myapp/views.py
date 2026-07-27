6dd4e6b01b679a0a1633ac3e0a237dcdd74fc230
Kaviyas-dev <kaviyakalaikumar2004>
2026-07-27 12:42:42 +0530

    Added update and delete functionality for students

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from myapp.forms import StudentForm, StudentCreationForm
 from myapp.models import Student
 
@@ -42,3 +42,22 @@ def add_student_modelform(request):
     else:
         form = StudentCreationForm()
     return render(request, "add-student-model.html", {"form": form})
+
+def update_student(request,id):
+    student=get_object_or_404(Student,id=id)
+    if request.method=="POST":
+        form=StudentCreationForm(request.POST, instance=student)
+        if form.is_valid():
+         form.save()
+         return redirect('students')
+    else:
+        form=StudentCreationForm(instance=student)
+    return render(request,'update-student.html',{'student':student,'form':form})
+
+def delete_student(request,id):
+    student=get_object_or_404(Student,id=id)
+    if request.method=="POST":
+        student.delete()
+        return redirect("students")
+    return render(request,"delete-student.html",{"student":student})
+
